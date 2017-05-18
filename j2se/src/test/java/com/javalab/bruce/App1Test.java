5063c041dd340b16b1f7e879c7da2bc52dc9ac7d
mcg7 <michael_c_global@sina.com>
2017-05-19 00:20:03 +0800

    增加了App1Test的解析

--- j2se/src/test/java/com/javalab/bruce/App1Test.java
+++ j2se/src/test/java/com/javalab/bruce/App1Test.java
@@ -19,26 +19,31 @@ package com.javalab.bruce;
  * 通过项目的pom.xml里声明了对junit的依赖,编译器能在classpath里找到junit-4.11.jar,
  * 在编译和打包的时候,JVM能加载到这个jar包。
  */
+
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;//静态成员导入,可以用略掉所在的类或接口名的方式，来使用静态成员。
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class App1Test {
 
   /**
-   * @Test 将一个普通的方法修饰成为一个测试方法.
+   * "@Test" 属于junit框架的注解:将一个普通的方法修饰成为一个测试方法.
    */
   @Test
-  public void greet() throws Exception {
+  public void greet() {
 
     /**
      * assertTrue与assertFalse可以判断某个条件是真还是假，如果和预期的值相同则测试成功，否则将失败。
      * 我们断言(或者说期望) App1.greet("first-app")的返回值是"first-app",
      * 也就是说,equals方法返回的boolean是true的话,assertTrue()通过,否则抛出Exception,表示测试不通过。
+     *
+     * equals方法用于比较两个String是否相等,注意不能用 == 比较两个字符串对象的值是否相等。为什么?
+     * equals的意义是什么?
      */
     assertTrue("first-app".equals(App1.greet("first-app")));
 
-    assertTrue("hello-world".equals(App1.greet("hello-world")));
+    assertFalse("hello".equals(App1.greet("hello-world")));
 
   }
 }
