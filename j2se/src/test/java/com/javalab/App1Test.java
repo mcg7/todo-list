4fcb367e7db106141578a42c9248bee0c4c10b36
apple <huxiaokang820@gmail.com>
2017-05-18 21:29:47 +0800

    second modify

--- j2se/src/test/java/com/javalab/App1Test.java
+++ j2se/src/test/java/com/javalab/App1Test.java
@@ -7,11 +7,12 @@ import static org.junit.Assert.assertTrue;
 public class App1Test {
     //定义App1Test类
     @Test
+    //注解Test
     public void greet() throws Exception {
-        //
+        //定义一个
         assertTrue("first-app".equals(App1.greet("first-app")));
-        //
-        assertTrue("hello-world".equals(App1.greet("hello")));
+        //静态方法assertTrue方法测试是true 则通过，比较字符串字面值和App1中静态方法中返回的值所包含的内容相同，如果测试不通过抛出错误。
+        assertTrue("hello-world".equals(App1.greet("hello-world")));
         //
     }
 
