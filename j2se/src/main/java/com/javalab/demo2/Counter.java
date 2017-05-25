488357f68f78d3194397ed6c6405969f66785f8a
apple <huxiaokang820@gmail.com>
2017-05-25 21:40:17 +0800

    first commit

--- j2se/src/main/java/com/javalab/demo2/Counter.java
+++ j2se/src/main/java/com/javalab/demo2/Counter.java
@@ -1,6 +1,8 @@
 package com.javalab.demo2;
 
 
+import java.io.*;
+
 public class Counter implements SalaryCalculator {
 
   /**
@@ -8,8 +10,33 @@ public class Counter implements SalaryCalculator {
    *
    * @return 雇员月薪之和
    */
-  public long calculateSalarySumOfContract() {
+    public static void main(String[] args) {
+      Counter c = new Counter();
+      System.out.println(c.calculateSalarySumOfContract());
 
-    return 0;
+    }
+  public long calculateSalarySumOfContract() {
+    long sum=0;
+    try {
+      FileReader fr = new FileReader("/Users/apple/java_lab/j2se/src/main/java/com/javalab/demo2/contracts.txt");
+      BufferedReader br = new BufferedReader(fr);
+      String name="";
+      long salary = 0;
+      String date = "";
+      String line = null;
+      String [] rec = null;
+      while ((line=br.readLine()) != null){
+        rec = line.split("\\,");
+        name = rec[0];
+        salary = Long.parseLong(rec[1]);
+        date = rec[2];
+        sum += salary;
+      }
+
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+
+    return sum;
   }
 }
